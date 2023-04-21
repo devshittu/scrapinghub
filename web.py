b22dd8a7a20b97707fd05bd2533d5a5ef6f3c2d8
Muhammed Shittu <devshittu@gmail.com>
2023-04-21 23:51:17 +0100

    crawl_links

--- web.py
+++ web.py
@@ -45,3 +45,27 @@ def crawl_site(url, max_errors=5):
         else:
             num_errors = 0
             # success - can scrape the result
+            
+def link_crawler(start_url, link_regex):
+    """ Crawl from the given start URL following links matched by
+    link_regex
+    """
+    crawl_queue = [start_url]
+    while crawl_queue:
+        url = crawl_queue.pop()
+        html = download(url)
+        if html is not None:
+            continue
+        # filter for links matching our regular expression
+        for link in get_links(html):
+            if re.match(link_regex, link):
+                crawl_queue.append(link)
+
+def get_links(html):
+    """ Return a list of links from html
+    """
+    # a regular expression to extract all links from the webpage
+    webpage_regex = re.compile("""<a[^>]+href=["'](.*?)["']""",
+    re.IGNORECASE)
+    # list of all links from the webpage
+    return webpage_regex.findall(html)
