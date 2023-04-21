0e89838c2a1d1be102ddabb85842bec481823b60
Muhammed Shittu <devshittu@gmail.com>
2023-04-21 23:32:25 +0100

    Download a web page and retries

--- web.py
+++ web.py
@@ -1,13 +1,17 @@
 import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
-def download(url):
+def download(url, num_retries=2):
     print(f"Downloading {url}")
     try:
         html = urllib.request.urlopen(url).read()
     except (URLError, HTTPError, ContentTooShortError) as e:
         print(f"Download error: {e.reason}")
         html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # recursively retry 5xx HTTP errors
+                return download(url, num_retries - 1)
     return html
  
 download('https://github.com/richardpenman/whois')
